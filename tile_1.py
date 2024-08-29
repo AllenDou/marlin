b5c382724b063549c4944a4305aa18ecdac70973
zixiao <shunli.dsl@alibaba-inc.com>
2024-08-29 14:37:04 +0800

    update.

--- tile_1.py
+++ tile_1.py
@@ -9,11 +9,13 @@ if True:
         for n in range(0, N, Ntile):
             for k in range(0, K):
                 # k loop should be outside of Mtile & Ntile,
-                # cause, Mtile & Ntile wile be load in-chip.
+                # cause, Mtile & Ntile will be load in-chip.
                 print(f"{k=}")
+                # load Mtile (from index k) from global memory into shared memory
                 for i in range(0, Mtile):
+                    # load Ntile (from index k) from global memory into shared memory
                     for j in range(0, Ntile):
                         row = m + i
                         col = n + j
                         #if m == 0 and n == 0:
-                        print(f"C[{row}][{col}] += A[{row}][{k}] * B[{k}][{col}];")                    
+                        print(f"C[{row}][{col}] += A[{row}][{k}] (mma) B[{k}][{col}];")
