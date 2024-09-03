710d63501bae7ed03ba4ebc77bd6f41185df7287
zixiao <shunli.dsl@alibaba-inc.com>
2024-09-03 22:03:51 +0800

    update.

--- test_1.cu
+++ test_1.cu
@@ -22,11 +22,26 @@ int stages = 4;
 int b_sh_wr_iters = 2;
 int thread_m_blocks = 4;
 
+// Instruction for loading a full 16x16 matrix fragment of operand A from shared memory, directly in tensor core layout.
+__device__ inline void ldsm4(FragA& frag_a, const void* smem_ptr) {
+  uint32_t* a = reinterpret_cast<uint32_t*>(&frag_a);
+  uint32_t smem = static_cast<uint32_t>(__cvta_generic_to_shared(smem_ptr));
+  asm volatile(
+    "ldmatrix.sync.aligned.m8n8.x4.shared.b16 {%0,%1,%2,%3}, [%4];\n"
+    : "=r"(a[0]), "=r"(a[1]), "=r"(a[2]), "=r"(a[3]) : "r"(smem)
+  );
+}
+
 int main() {
   /*Vec<half2, 4>*/ FragA frag_a[2][thread_m_blocks/*4*/];
   /*Vec<int, 4>*/   I4 frag_b_quant[2];
   /*Vec<float, 4>*/ FragC frag_c[thread_m_blocks/*4*/][4][2];
   /*Vec<half2, 1>*/ FragS frag_s[2][4];
+
+  //int data[] = {1, 2, 3, 4};
+  //ldsm4(frag_a[0][0], data);
+
+  return 0;
 
   while (slice_iters/* one iter for a tile */) {
     // We unroll over both the global fetch and the register load pipeline to ensure all shared memory accesses are
