c7d0b0db3fcbdfa47f8230af4043be69c0924ec1
zixiao <shunli.dsl@alibaba-inc.com>
2024-09-09 11:21:56 +0800

    update.

---
+++ run_jupyter.sh
@@ -0,0 +1,1 @@
+jupyter-lab --allow-root --no-browser --NotebookApp.token=emr-datascience --NotebookApp.password=emr-datascience --notebook-dir=$PWD --port=8000 --NotebookApp.allow_origin=* --ip 0.0.0.0
