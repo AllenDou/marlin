1a52658790b599d869f18745b98aaee42bab6a29
zixiao <shunli.dsl@alibaba-inc.com>
2024-09-10 11:59:31 +0800

    update.

--- tile_6.py
+++ tile_6.py
@@ -5,7 +5,8 @@ b_sh_wr_iters = 2
 
 def start_pipes() :
     for i in range(0, stages - 1):
-        print(f"fetch_to_shared pipe={i} off={i} B_ptr/s_gl_rd incr")
+        print(f"fetch_to_shared(commit group) pipe={i} off={i} B_ptr/s_gl_rd incr")
+    print("wait_for_stages(wait group 2)")
     print("fetch_to_register iter=0 pipe=0")
     print("a_gl_rd incr")
 
@@ -18,9 +19,9 @@ while (slice_iters):
         for k in range(0, b_sh_wr_iters):
             print(f"fetch_to_register next_iter={(k+1) % b_sh_wr_iters} pipe={pipe % stages}")
             if k == b_sh_wr_iters - 2:
-                print(f"fetch_to_shared next_pipe={(pipe + stages - 1) % stages} off={pipe} B_ptr/s_gl_rd incr")
+                print(f"fetch_to_shared(commit group) next_pipe={(pipe + stages - 1) % stages} off={pipe} B_ptr/s_gl_rd incr")
                 pipe += 1
-                print(f"wait_for_stages(2)")
+                print(f"wait_for_stages(wait group 2)")
             print(f"{slice_iters=} matmul(iter={k})")
         slice_iters -= 1 
         if slice_iters == 0:
