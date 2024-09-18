f1e174896904c42b2932f198cc20bc12c7f0b455
zixiao <shunli.dsl@alibaba-inc.com>
2024-09-18 14:39:56 +0800

    update.

--- help_abs_index.py
+++ help_abs_index.py
@@ -19,6 +19,8 @@ a_sh_wr_iters = 2
 a_sh_wr_delta = 256
 a_sh_rd_delta_o = 4
 a_sh_rd_delta_i = 128
+threads = 256
+a_gl_rd_delta_i = a_gl_stride * int(threads / a_gl_rd_delta_o)
 
 
 def transform_a(i):
@@ -57,7 +59,7 @@ for threadIdx_x in range(256):
     s_sh_wr = threadIdx_x
     s_sh_rd = 8 * (int(threadIdx_x / 32) % int(thread_n_blocks / 4)) + int((threadIdx_x % 32) / 4)
     #s_sh_rd = 8 * (int(threadIdx_x / 32) % int(thread_n_blocks / 4)) + int((threadIdx_x % 32) % 4)
-    print(f"{threadIdx_x:3}| {a_gl_rd=:5} {a_sh_wr=:3} a_sh_wr_trans={a_sh_wr_trans0:3}/{a_sh_wr_trans1:3} {a_sh_rd=:3} \
+    print(f"{threadIdx_x:3}| a_gl_rd={a_gl_rd:5}/{a_gl_rd_delta_i}+{a_gl_rd:5} {a_sh_wr=:3} a_sh_wr_trans={a_sh_wr_trans0:3}/{a_sh_wr_trans1:3} {a_sh_rd=:3} \
 a_sh_rd_trans={a_sh_rd_trans00:3}/{a_sh_rd_trans01:3}/{a_sh_rd_trans02:3}/{a_sh_rd_trans03:3}/{a_sh_rd_trans10:3}/{a_sh_rd_trans11:3}/{a_sh_rd_trans12:3}/{a_sh_rd_trans13:3} \
 |{b_gl_rd=:4} {b_sh_wr=:3} {b_sh_rd=:3} |{s_gl_rd=:3} {s_sh_wr=:3} \
 {s_sh_rd=:2} tid={threadIdx_x:3}")
