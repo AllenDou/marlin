afe03cf729774f9c683b42c640b691d4159d5788
zixiao <shunli.dsl@alibaba-inc.com>
2024-11-28 19:17:36 +0800

    update.

--- test.py
+++ test.py
@@ -165,9 +165,9 @@ class Test(unittest.TestCase):
             for threadidx in range(0,user_specified_threadidx ):
                 #for m in [16]:
                 #for m in [1024]:
-                for m in [2048]:
+                for m in [8]:
                     for groupsize in [128]:
-                        for n, k in [(4096, 4096)]:
+                        for n, k in [(5120, 51200)]:
                             for thread_shape in [(64, 256)]:
                                 self.run_problem(m, n, k, *thread_shape, groupsize, print_enable, blockidx, threadidx)
 
